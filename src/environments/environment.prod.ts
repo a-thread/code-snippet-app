501b0876f91ee05ec243950a6d11465beb3b8d8e
Aiden Threadgoode <aiden.threadgoode@kehe.com>
2024-12-27 21:18:23 -0500

    adding renderer url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     githubClientId: 'Ov23liLReAbq1vtPY5UP',
     githubRedirectUri: 'https://a-thread.github.io/gist-app/callback',
-    authApiUrl: 'http://localhost:3000/api',
+    authApiUrl: 'https://gist-app-ik73.onrender.com',
 };
